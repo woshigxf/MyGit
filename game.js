175a964f9f89e48969e0ed329ccce8f73cab635f
Micheal_Gao <woshigxf@qq.com>
2018-11-14 11:43:04 +0800

    优化猜拳小游戏页面效果及代码

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+//获取对象
 var oSelect = document.getElementsByClassName("select")[0];
 var oResult = document.getElementsByClassName("result")[0];
 var oJd = document.getElementsByClassName("jd")[0];
@@ -9,70 +10,60 @@ var oNpc = document.getElementsByClassName("npc")[0];
 var oBtn = document.getElementById("btn");
 var oCount = document.getElementById("count");
 
-var nSelect = "";
-var num = 0;
+//初始化 玩家数据、电脑数据以及胜利次数
+var nSelect = 0, nComputer = 0, num = 0;
+
+//事件绑定
 oJd.onclick = function(){
     nSelect = 1;
-    var nComputer = Math.floor(Math.random()*3 + 1);
+    nComputer = nRandom();
     caiquan(nSelect,nComputer);
 }
 oSt.onclick = function(){
     nSelect = 2;
-    nComputer = Math.floor(Math.random()*3 + 1);
+    nComputer = nRandom();
     caiquan(nSelect,nComputer);  
 }
 oBu.onclick = function(){
     nSelect = 3;
-    nComputer = Math.floor(Math.random()*3 + 1); 
+    nComputer = nRandom(); 
     caiquan(nSelect,nComputer);
-}
-function caiquan(nSelect,nComputer){
-    bijiao(nSelect,nComputer);
-    oSelect.style.display="none";
-    oResult.style.display="flex";
-    oBtn.style.display="block";
-    oPlayer.innerHTML = shoushi(nSelect);
-    oFinal.innerHTML = bijiao(nSelect,nComputer);
-    oNpc.innerHTML = shoushi(nComputer);
-    if(bijiao(nSelect,nComputer) == "胜"){
-        num++;
-    }
-    oCount.innerHTML = num;   
 }
 oBtn.onclick = function(){
     oSelect.style.display = "flex";
     oResult.style.display = "none";
     this.style.display = "none";
 }
+
+//创建功能函数
+//获取电脑出的拳，创建一个1到3随机数
+function nRandom(){
+    return Math.floor(Math.random()*3 + 1);
+}
+//根据玩家和电脑的数据来修改页面样式
+function caiquan(nSelect,nComputer){
+    var nfinal = bijiao(nSelect,nComputer);
+    oSelect.style.display="none";
+    oResult.style.display="flex";
+    oBtn.style.display="block";
+    oPlayer.className = oNpc.className = oFinal.className = "";
+    oPlayer.className = "player b"+nSelect;
+    oFinal.className = "final b"+nfinal;
+    oNpc.className = "npc b"+nComputer;
+    if(nfinal == 1){
+        num++;
+    }
+    oCount.innerHTML = num;   
+}
+//计算猜拳结果，返回相应数值
 function bijiao(player,npc){
+    // res的值 1:胜 2:平 3:负
     if(player == npc){
-        var res = "平";
+        var res = 2;
     }else if(player < npc){
-        if(player==1&&npc==3){
-           res = "胜";
-        }else{
-            res = "负";
-        }
+        res = player==1&&npc==3?1:3;
     }else{
-        if(player==3&&npc==1){
-            res="负";
-        }else{
-            res = "胜";
-        }
+        res = player==3&&npc==1?3:1;
     }
     return res;
-}
-function shoushi(val){
-    switch (val) {
-        case 1:
-            var hand = "剪刀";
-            break;
-        case 2:
-            hand = "石头";
-            break;    
-        default:
-            hand = "布";
-            break;
-    }
-    return hand;
 }
